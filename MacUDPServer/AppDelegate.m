14cc434f7bb3089fe8d1ba1372372b5ee0e193b7
Akihiro Okubo <alchemy.theory@gmail.com>
2014-06-22 01:41:25 +0900

    Initial Commit

---
+++ MacUDPServer/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  MacUDPServer
+//
+//  Created by Akihiro Okubo on 2014/06/22.
+//  Copyright (c) 2014年 Akihiro Okubo. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
